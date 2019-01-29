a13e367f0ec08f1b6908388956d7f6c33b9a9523
Márcio Oliveira <emaildomso@gmail.com>
2019-01-29 11:07:40 -0200

    feat: Exercise 4 completed
    - Webview loads URL from flavor
    - SwipeRefresh reloads WebView

--- app/src/main/java/com/smartconsultingchallenge/Exercise4Fragment.java
+++ app/src/main/java/com/smartconsultingchallenge/Exercise4Fragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Exercise4Fragment extends Fragment {
 
@@ -18,10 +20,27 @@ public class Exercise4Fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+        View view = inflater.inflate(R.layout.fragment_exercise4, container, false);
 
-        TextView message = view.findViewById(R.id.message);
-        message.setText(String.format("%s - %s", getActivity().getString(R.string.title_exercise_4), BuildConfig.URL));
+        final SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.swipe_refresh);
+        final WebView webView = view.findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.loadUrl(BuildConfig.URL);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.loadUrl(BuildConfig.URL);
+            }
+        });
 
         return view;
     }
